f30c87b3fb3bf77bf245c3e0e3ab7e8cc9f30193
Jack Fok <jack.fok@qbssystem.com>
2019-09-23 17:42:33 +0800

    spring cloud tutorial

---
+++ microservices/netflix-eureka-naming-server/src/test/java/com/example/microservies/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservies.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
